7bb83ec3b8b23cafddf39ad2b1cbfb57a0c74389
Matias Salas <mat.salas@comcast.net>
2017-03-26 13:19:17 -0700

    initial methods for getting gpio states.

--- public/main.js
+++ public/main.js
@@ -5,21 +5,30 @@ var raspberry_pi = function () {
   var socket = io();
 
   //  get gpio status from server
-  socket.on('get_io', function (io_state) {
-    document.getElementById('io_17').checked = io_state;
+  // socket.on('get_io', function (io_state) {
+  //   document.getElementById('io_17').checked = io_state;
+  // });
+
+  socket.on('input_2', function (io_state) {
+    document.getElementById('io_2').checked = !io_state;
   });
 
   //  method event to toggle gpio output #
-  var toggle_io = function (io) {
-    socket.emit('toggle', io);
-  };
+  // var toggle_io = function (io) {
+  //   socket.emit('toggle_17', io);
+  // };
 
-  document.addEventListener('keydown', function (e) {
-    var key_name = e.key;
+  // document.addEventListener('keydown', function (e) {
+  //   var key_name = e.key;
 
-    if (key_name === 'Control') {
-      toggle_io(17);
-    }
+  //   if (key_name === 'Control') {
+  //     toggle_io(17);
+  //   }
+  // });
+
+  document.getElementById('io_17').addEventListener('click', function (e) {
+    //console.log(io_17.checked);
+    socket.emit('input_17', io_17.checked);
   });
 
 };
--- server.js
+++ server.js
@@ -17,25 +17,79 @@ var onoff = require('onoff');
 // ***** onoff module Raspberry PI GPIO *****
 var Gpio = onoff.Gpio;
 
-var led = new Gpio(17, 'out');
+var led = null;
 var interval;
 var array_io = [];
+var io_item = {};
+var index_offset = 2;
+var button2;
+
+//  build array io
+
+var get_gpio_states = function () {
+  for (i = 2; i <= 27; i++) {
+    //var led = new Gpio(i, 'out');
+    array_io.push(io_item = {
+      name: 'io_' + i,
+      pin_out: i,
+      state: new Gpio(i, 'out')
+    });
+  }
+  button2 = new Gpio(2, 'in', 'both');
+};
+// initiate gpio states
+//get_gpio_states();
+
 
 io.on('connection', function (socket) {
-  interval = setInterval(function () {
-    var value = (led.readSync() + 1) % 2;
-    led.write(value, function () {
-      console.log('Changed LED state to: ' + value);
-      socket.broadcast.emit('get_io', value);
+  get_gpio_states();
+  //interval();
+  //  blink gpio 17 example =========================
+  // io_17_blink = setInterval(function () {
+  //   console.log(array_io[17 - index_offset].name);
+  //   var value = (array_io[17 - index_offset].state.readSync() + 1) % 2;
+  //   array_io[17 - index_offset].state.write(value, function () {
+  //     console.log('Changed LED ' + array_io[17 - index_offset].name + ' state to: ' + value);
+  //     socket.broadcast.emit('get_io', value);
+  //   });
+  // }, 2000);
+  // =================================================
+
+  socket.on('input_17', function (value) {
+    //  check if value is true, set value for onoff
+    value = value ? 1 : 0;
+    array_io[17 - index_offset].state.write(value, function () {
+      //  todo
     });
-  }, 2000);
+  });
+
+  // get gpio 2 input
+  button2.watch(function (err, value) {
+    if (err) {
+      // TODO handle errors
+      //throw err;
+    }
+    //console.log(value);
+    socket.broadcast.emit('input_2', value);
+  });
+
+  socket.on('disconnect', function () {
+    //release_gpio();
+    console.log('Bye!');
+  });
 });
 
+var release_gpio = function () {
+  for (i = 0; i <= 25; i++) {
+    array_io[i].state.writeSync(0);
+    array_io[i].state.unexport();
+  }
+  button2.unwatch();
+};
 
 process.on('SIGNT', function() {
-  clearInterval(interval);
-  led.writeSync(0);
-  led.unexport();
+  clearInterval(io_17_blink);
+  release_gpio();
   console.log('Bye!');
   process.exit();
 });
